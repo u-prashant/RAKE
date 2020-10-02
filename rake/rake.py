86dcf3778d4d67f784d5fb21a10925c6ba432d0e
Prashant Upadhyay <prashant.upadhyay@privafy.com>
2020-10-02 09:46:35 +0530

    Added requirements, travis and makefile and corrected code style

--- rake/rake.py
+++ rake/rake.py
@@ -1,6 +1,7 @@
 import re
 import operator
 
+
 def load_stop_words(stop_words_file_path: str) -> list:
     """Loads stop words from a file and return as a list of words.
 
@@ -11,7 +12,7 @@ def load_stop_words(stop_words_file_path: str) -> list:
     file = open(stop_words_file_path)
     for line in file:
         if line.strip()[0:1] != "#":
-            for word in line.split(): # in case more than one per line
+            for word in line.split():  # in case more than one per line
                 stop_words.append(word)
     file.close()
     return stop_words
@@ -28,42 +29,51 @@ def build_stop_word_regex(stop_words_file_path: str) -> re.Pattern:
     for word in stop_words_list:
         word_regex = r'\b' + word + r'(?![\w-])'
         stop_words_regex_list.append(word_regex)
-    stop_words_pattern = re.compile('|'.join(stop_words_regex_list), re.IGNORECASE)
+    stop_words_pattern = re.compile('|'.join(stop_words_regex_list),
+                                    re.IGNORECASE)
 
     return stop_words_pattern
 
 
 class RAKE(object):
-    def __init__(self, stop_words_file_path: str = 'resources/SmartStoplist.txt'):
-        self.stop_words_file_path = stop_words_file_path
-        self.stop_words_pattern = build_stop_word_regex(stop_words_file_path)
+    def __init__(self, stop_words_file: str = 'resources/SmartStoplist.txt'):
+        self.stop_words_file_path = stop_words_file
+        self.stop_words_pattern = build_stop_word_regex(stop_words_file)
 
     def exec(self, text: str):
         sentences = self.split_sentences(text)
         phrases = self.generate_candidate_keywords(sentences)
         word_scores = self.calculate_word_scores(phrases)
-        keyword_candidates = self.generate_candidate_keyword_scores(phrases, word_scores)
-        sorted_keywords = sorted(keyword_candidates.items(), key=operator.itemgetter(1), reverse=True)
-        
+        keyword_candidates = self.generate_candidate_keyword_scores(
+                                                                phrases,
+                                                                word_scores)
+        sorted_keywords = sorted(keyword_candidates.items(),
+                                 key=operator.itemgetter(1),
+                                 reverse=True)
+
         return sorted_keywords
 
     def split_sentences(self, text: str) -> list:
         """Split text into sentences."""
-        sentence_delimiters = re.compile(u'[.!?,;:\t\\\\"\\(\\)\\\'\u2019\u2013]|\\s\\-\\s')
+        pattern = u'[.!?,;:\t\\\\"\\(\\)\\\'\u2019\u2013]|\\s\\-\\s'
+        sentence_delimiters = re.compile(pattern)
         sentences = sentence_delimiters.split(text)
 
         return sentences
 
     def generate_candidate_keywords(self, sentences: list) -> list:
-        """Returns keyword phrases after removing stopwords from each sentence."""
+        """
+        Returns keyword phrases after removing stopwords from each sentence.
+        """
         phrases_list = []
         for sentence in sentences:
-            phrases = re.sub(self.stop_words_pattern, '|', sentence.strip()).split('|')
-            for phrase in phrases: 
+            phrases = re.sub(self.stop_words_pattern, '|',
+                             sentence.strip()).split('|')
+            for phrase in phrases:
                 phrase = phrase.strip().lower()
                 if phrase != "":
                     phrases_list.append(phrase)
-        
+
         return phrases_list
 
     def is_number(self, s):
@@ -74,19 +84,20 @@ class RAKE(object):
             return False
 
     def separate_words(self, text: str, word_min_size: int = 0) -> list:
-        """Return a list of all words of length greater than specified min size.
+        """
+        Return a list of all words of length greater than specified min size.
 
         Keyword arguments:
         text -- the text that is to be split into words
-        word_min_size -- the min. no. of characters a word must have (default 0)
+        word_min_size -- the min. no. of characters a word must have (def: 0)
         """
         splitter = re.compile('[^a-zA-Z0-9_\\+\\-/]')
         words = []
         for single_word in splitter.split(text):
             current_word = single_word.strip().lower()
             if len(current_word) > word_min_size \
-                and current_word != '' \
-                and not self.is_number(current_word):
+                    and current_word != '' \
+                    and not self.is_number(current_word):
                 words.append(current_word)
 
         return words
@@ -110,12 +121,13 @@ class RAKE(object):
         # Calculate word score = def(w) / freq(w)
         word_score = {}
         for item in word_frequency:
-            #word_score.setdefault(item, 0):
-            word_score[item] = word_degree[item] / (word_frequency[item] * 1.0)
+            # word_score.setdefault(item, 0):
+            word_score[item] = word_degree[item] / (word_frequency[item]*1.0)
 
         return word_score
 
-    def generate_candidate_keyword_scores(self, phrases: list, word_score: dict) -> dict:
+    def generate_candidate_keyword_scores(self, phrases: list,
+                                          word_score: dict) -> dict:
         """Returns the dict. of candidate keywords with scores."""
         keyword_candidates = {}
         for phrase in phrases:
@@ -125,5 +137,5 @@ class RAKE(object):
             for word in words:
                 candidate_score += word_score[word]
             keyword_candidates[phrase] = candidate_score
-        
+
         return keyword_candidates
--- tests/rake_test.py
+++ tests/rake_test.py
@@ -1,33 +1,36 @@
-import unittest, os, sys
-sys.path.append(os.path.abspath(sys.path[0]) + '/../')
+import unittest
+import os
+import sys
 from rake.rake import RAKE
+
+sys.path.append(os.path.abspath(sys.path[0]) + '/../')
+
 
 class TestRAKE(unittest.TestCase):
 
     def setUp(self):
         self.rake = RAKE('rake/resources/SmartStoplist.txt')
-    
 
     def test_exec(self):
         text = """
-               Keyword extraction is not that difficult after all.
-               There are many libraries that can help you with keyword extraction.
-               Rapid automatic keyword extraction is one of those.
-               """
-        expected_scores = [('rapid automatic keyword extraction', 13.333333333333332), 
-                           ('keyword extraction', 5.333333333333333), 
-                           ('difficult', 1.0), 
-                           ('libraries', 1.0)]
+        Keyword extraction is not that difficult after all.
+        There are many libraries that can help you with keyword extraction.
+        Rapid automatic keyword extraction is one of those.
+        """
+        expected_scores = [
+            ('rapid automatic keyword extraction', 13.333333333333332),
+            ('keyword extraction', 5.333333333333333),
+            ('difficult', 1.0),
+            ('libraries', 1.0)]
         actual_scores = self.rake.exec(text)
         self.assertEqual(expected_scores, actual_scores)
 
-        expected_scores = [('difficult', 1.0), 
-                           ('libraries', 1.0)]
+        expected_scores = [('difficult', 1.0), ('libraries', 1.0)]
         self.assertNotEqual(expected_scores, actual_scores)
 
-# if __name__ == '__main__':
-#     unittest.main()
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestRAKE)
 unittest.TextTestRunner(verbosity=2).run(suite)
-        
+
+# if __name__ == '__main__':
+#     unittest.main()
